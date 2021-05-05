0a5e5da397ba27711c8372a6179ab951e87bc7a8
Matin Daghyani <59438691+mtndaghyani@users.noreply.github.com>
2021-05-05 14:03:00 +0430

    Update server.py

--- server.py
+++ server.py
@@ -15,15 +15,21 @@ def handle_client_message(client, message):
     global users
     message_params = message.split("/")
     if message_params[0] == "join":
-        users.append(message_params[1])
+        users.append(message_params[1] + " > " + "online")
         server_response = "You joined successfully!"
     elif message_params[0] == "send":
         messages.append(message_params[1] + " > " + message_params[2])
         server_response = "Message sent."
     elif message_params[0] == "users":
-        server_response = " ".join(users)
+        server_response = "\n".join(users)
     elif message_params[0] == "refresh":
         server_response = "\n".join(messages)
+    elif message_params[0] == "exit":
+        for i in range(len(users)):
+            if users[i].startswith(message_params[1]):
+                users[i] = message_params[1] + " > " + "offline"
+        server_response = "Client logged out successfully."
+
     else:
         server_response = "INVALID INSTRUCTION."
     client.send(server_response.encode())
